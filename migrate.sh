c39f9b600e46b9d504c9c6e7cd0ac2382ef2ecf7
Ayush_Saini <73630171+Ayushsaini20@users.noreply.github.com>
2024-12-26 16:03:35 +0530

    Update migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -1,65 +1,54 @@
-#!/bin/bash
-
-# Mapper
+# Migrate Repo from BitBucket to GitHub Enterprise
+ 
+## Mapper
 # BitBucket Workspace -> GitHub Enterprise
 # BitBucket Project -> GitHub Organization
 # BitBucket Repository -> GitHub Repository
-
-
-BB_WORKSPACE=bb_to_gh
-BB_PROJECT=test
-BB_REPO=test_repo1
-GH_REPO=bbtest2
+ 
+BB_WORKSPACE=noicecurse
+BB_PROJECT=org
+BB_REPO=repo
+#GITHUB_REMOTE_URL=https://x-access-token:$GHE_TOKEN@github.com/nishkarshraj/23-Dec-8
+GITHUB_REMOTE_URL=https://X-ACCESS-TOKEN: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@github.com/Ayushsaini20/newtest_repo.git
+GH_REPO="newtest_repo"
 GH_USERNAME=Ayushsaini20
 GH_EMAIL=ayushsaini963@gmail.com
-
-# Echo the credentials (for debugging, not recommended in production)
-echo "BitBucket Token: $BB_TOKEN"
-echo "GitHub Token: $GHE_TOKEN"
-echo "Git Clone URL: https://x-token-auth:$BB_TOKEN@bitbucket.org/$BB_WORKSPACE/$BB_REPO.git"
-
+ 
 echo "Cloning BitBucket Repository into GitHub Runner Context"
-git clone https://x-token-auth:$BB_TOKEN@bitbucket.org/$BB_WORKSPACE/$BB_REPO.git
-if [ $? -ne 0 ]; then
-    echo "Failed to clone BitBucket repository. Check credentials and repository details."
-    exit 1
-fi
-
-cd $BB_REPO
-
-
+git clone https://x-token-auth:ATCTT3xFfGN0Ve3CQ6yEbGWRfiZ_Gq2bha6lTPQfAKlseqtlRYOSvTMArZTx5wUvab8uBly5ER_KolfsdyCYnJO7JYHORPgIIm8jQgIsmkM4oNWsXbFnvCJUj325QvQUFRok3AK1H4ptGl_6jVXXxhL0feKr7bb2lCQVd5XmbQ9_Gqs7sr5qG7c=C7638B80@bitbucket.org/bb_to_gh/test_repo1.git
+cd test_repo1
+ 
 echo "Creating GitHub Repository with GH APIs"
+ 
 curl -X POST https://api.github.com/user/repos \
 -H "Authorization: Bearer $GHE_TOKEN" \
 -H "Accept: application/vnd.github+json" \
 -d "{
   \"name\": \"$GH_REPO\",
-  \"description\": \"Test Repo migrated from BitBucket\",
+  \"description\": \"Test Repo\",
   \"private\": true
 }"
-if [ $? -ne 0 ]; then
-    echo "Failed to create GitHub repository. Check GitHub credentials and API permissions."
-    exit 1
-fi
-
+ 
+ 
 echo "Adding GitHub remote..."
-git config user.name $GH_USERNAME
-git config user.email $GH_EMAIL
-git remote add github "https://x-access-token:$GHE_TOKEN@github.com/$GH_USERNAME/$GH_REPO"
+git config user.name NishkarshRaj
+git config user.email nishkarshraj000@gmail.com
+git remote add github "$GITHUB_REMOTE_URL"
+ 
 if [ $? -ne 0 ]; then
     echo "Failed to add GitHub remote. Exiting."
     exit 1
 fi
-
+ 
+# Step 3: Push all branches and tags to GitHub
 echo "Pushing to GitHub..."
 git push --mirror github
+ 
 if [ $? -ne 0 ]; then
     echo "Failed to push to GitHub. Exiting."
     exit 1
 fi
-
+ 
 echo "Cleaning up Workspace"
 cd ..
-#rm -rf $BB_REPO
-
-echo "Migration Completed Successfully!"
+rm -rf $BB_REPO
